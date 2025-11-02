5cd940c3250af2357095c0f5df59205319c955fd
Sasha2799-hub <alex0kuz99@gmail.com>
2025-11-02 17:31:51 +0200

    add hw2

---
+++ src/components/App/App.tsx
@@ -0,0 +1,63 @@
+import css from "./App.module.css";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
+import { useState } from "react";
+import { Votes, VoteType } from "../../types/votes";
+
+export default function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const [isResetBtn, setResetBtn] = useState<boolean>(false);
+
+  const handleVote = (type: VoteType) => {
+    setVotes({
+      ...votes,
+      [type]: votes[type] + 1,
+    });
+    setResetBtn(true);
+  };
+
+  let totalVotes: number = Object.values(votes).reduce(
+    (acc, current) => acc + current,
+    0
+  );
+
+  let rateVotes: number = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+    setResetBtn(false);
+  };
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={isResetBtn}
+      />
+      {totalVotes ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={rateVotes}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+}
